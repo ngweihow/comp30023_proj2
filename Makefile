e68199305eeb40e1067770ffe1f3ad5e2714a437
rene <ngweihow.r@gmail.com>
2018-05-13 14:21:02 +1000

    Updated Makefile and basic structure

---
+++ Makefile
@@ -0,0 +1,23 @@
+#     Standard Make File
+#     +------------------------------------------------------------------+
+#     | Project Code for Computer Systems COMP30023 2018 S1              |
+#     | A simple program to validate TLS Certificate Files               |
+#     | To compile: Run the Makefile with "make"                         |
+#     | Written by: Wei How Ng (828472) wein4                            |
+#     +------------------------------------------------------------------+
+#
+
+CC = gcc
+CFLAGS = -Wall -lssl -lcrypto
+EXE = certcheck
+OBJ = certcheck.c
+
+$(EXE): $(OBJ)
+	$(CC) -o $(EXE) $(OBJ) $(CFLAGS)
+
+clean: rm -f $(EXE)
+
+
+scp:
+	scp *.c Makefile ubuntu@115.146.85.92:./comp30023_all/comp30023_p2/
+
---
+++ certcheck.c
@@ -0,0 +1,58 @@
+/*   Main CertCheck File
+     +------------------------------------------------------------------+
+     | Project Code for Computer Systems COMP30023 2018 S1              |
+     | A simple program to validate TLS certificate files               |
+     | To compile: Run the Makefile with "make"                         |
+     | Written by: Wei How Ng (828472) wein4                            |
+     +------------------------------------------------------------------+
+
+ */
+
+
+// Library and header includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/x509.h>
+#include <openssl/x509v3.h>
+#include <openssl/bio.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
+
+// Importation of header files 
+
+
+
+
+
+// ----------------------------------------------------------------------
+/* Main Function
+ *
+ */
+
+int 
+main(int argc, char *argv[])
+{   
+
+    // Initialisation of the certificates 
+    BIO *certificate_bio = NULL;
+    X509 *cert = NULL;
+    X509_NAME *cert_issuer = NULL;
+    X509_CINF *cert_inf = NULL;
+    STACK_OF(X509_EXTENSION) * ext_list;
+
+    // Initialise openSSL
+    OpenSSL_add_all_algorithms();
+    ERR_load_BIO_strings();
+    ERR_load_crypto_strings();
+
+    // Create BIO object to read certificate
+    certificate_bio = BIO_new(BIO_s_file());
+
+
+
+
+    return 0;
+}
+
+// ----------------------------------------------------------------------
